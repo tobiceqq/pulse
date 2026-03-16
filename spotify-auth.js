65f6cb1921c44ee9d1a6be151dcc96848799c8ae
tobiceqq <bires@spsejecna.cz>
2026-03-16 19:03:17 +0100

    updated spotify-auth.js

--- spotify-auth.js
+++ spotify-auth.js
@@ -51,6 +51,7 @@ export async function startLogin({ clientId, redirectUri, scopes }) {
     scope: scopes.join(" "),
     code_challenge_method: "S256",
     code_challenge: challenge,
+    show_dialog: "true",
   });
 
   window.location.href = `https://accounts.spotify.com/authorize?${params.toString()}`;
